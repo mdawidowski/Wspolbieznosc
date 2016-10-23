4e6a73760637370f307b95bdddbd95d8ce73bb4d
Marcin Dawidowski <mdawidowski@sigma.ug.edu.pl>
2016-10-23 10:57:16 +0200

    Create serwer.sh

---
+++ Lab1/Zadanie/serwer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while [ 1 ]
+do
+  if [ -e "dane.txt" ]
+  then
+    liczba=$(cat "dane.txt")
+  fi
+
+  if [ -e "dane.txt" ]
+    then
+        rm dane.txt
+  fi
+  let wynik=liczba*5;
+  echo $wynik > wyniki.txt
+done
