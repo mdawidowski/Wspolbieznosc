93c67c9e892666828f4dc041f671d8f75147af07
Marcin Dawidowski <mdawidowski@sigma.uge.du.pl>
2016-11-23 12:11:29 +0100

    tak jak byc powinno

--- Lab3/Zad.sh
+++ Lab3/Zad.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
-declare -i licznik
+declare -i x
 
 dir=$(pwd)
 
 if [ -d "$1" ]; then
   sciezka="$dir/$1"
   cd $sciezka
-  for i in $( tree ); do
+  for i in $( ls ); do
     if [ "$i" == "$2" ] ; then
-      let "licznik += 1"
+      let "x += 1"
+    else
+      while [[ -d $i ]]; do
+        if [[ -d $i ]]; then
+          cd $i
+        fi
+        for z in $( ls ); do
+          if [[ "$z" == "$2" ]]; then
+            let "x += 1"
+          fi
+          for i in $( ls ); do
+            sleep 0
+          done
+         if [[ $i == $2 ]]; then
+           let "x += 1"
+         fi
+      done
+      done
     fi
   done
 else
-  echo "Podany parametr jest niepoprawny"
+  echo "podaj prawidlowy parametr"
 fi
 
 if [ -z $3] ; then
-  if [ -z $licznik ] ; then
-    echo "Niestety nie znaleziono"
+  if [ -z $x ] ; then
+    echo "Niestety niczego nie znaleziono"
   else
-    echo "Plik zostal znaleziony"
+    echo "Plik/katalog zostal znaleziony"
   fi
 fi
