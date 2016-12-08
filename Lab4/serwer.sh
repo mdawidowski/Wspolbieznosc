39a33a8aa9cb2fcf818ba0098bda8e84af029878
Marcin Dawidowski <mdawidowski@sigma.uge.du.pl>
2016-12-08 21:53:00 +0100

    Lab4 gotowe

--- Lab4/serwer.sh
+++ Lab4/serwer.sh
@@ -5,6 +5,7 @@ trap "exit 0" SIGUSR1
 
 klient=klientfifo
 serwer=serwerfifo
+
 if [[ -e $serwer ]]; then
   rm $serwer
   mkfifo $serwer
@@ -13,12 +14,19 @@ else
   mkfifo $serwer
   chmod 777 $serwer
 fi
-
+echo to jest $1
 while [[ true ]]; do
-  read line < $serwer
-  echo $line
-  line=$(sed 's/[^0-9]//g' <<< $line)
-  x=$line
-  let $[x=x+1]
-  echo $x > klientfifo
+  if [[ -z $1 ]]; then
+    read line < $serwer
+    echo $line
+    line=$(sed 's/[^0-9]//g' <<< $line)
+    x=$line
+    echo $0 $x
+    /bin/bash $0 $x
+  else
+    x=$1
+    let $[x=x+1]
+    echo $x > klientfifo
+    exit 0
+  fi
 done
